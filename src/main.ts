63a6051429967792791363161a0b588439b80287
Richie Wee <ricia1212@gmail.com>
2022-06-10 15:31:08 +0800

    Heroku quick fixes for deploy

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
-  await app.listen(process.env.PORT || 8080);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
