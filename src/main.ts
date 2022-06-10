b8d5f18165fc44d08efb2dbb531b28ebbfc02855
Richie Wee <ricia1212@gmail.com>
2022-06-10 10:28:21 +0800

    AWS config added

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
